6393ad760832b6da18856ea211e0fde22ed053ed
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-11-01 17:15:40 -0300

    Correçoes de warning

--- src/Componentes/Cards/CardHorizontal/index.js
+++ src/Componentes/Cards/CardHorizontal/index.js
@@ -1,27 +1,22 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 
 const CardHorizontal = (props) => {
+  return (
+    <div className=" card containerCardHorizontal ">
+      <div>
+        <img className="containerFotoHrizontal" src={props.img} alt="" />
+      </div>
 
-    return (
-        <div className=" card containerCardHorizontal ">
-                <div>
-                    <img className="containerFotoHrizontal" src={props.img}/>
-                </div>
+      <div className="card-content conatinerTextHorizontal">
+        <div class="cardTitleHorizontal ">{props.title}</div>
 
-                <div className='card-content conatinerTextHorizontal'>
-                    <div class="cardTitleHorizontal ">
-                        {props.title}
-                    </div>
-                   
-
-                    <div className="card-content">
-                        <p>{props.descricao}</p>
-                    </div>
-
-                </div>
+        <div className="card-content">
+          <p>{props.descricao}</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default CardHorizontal;
--- src/Componentes/Cards/CardVertical/index.js
+++ src/Componentes/Cards/CardVertical/index.js
@@ -1,30 +1,24 @@
 import React from "react";
-import './index.css';
-
+import "./index.css";
 
 const CardVertical = (props) => {
+  return (
+    <div className=" contaiinerCard">
+      <div className="col s12 m7 contaiinerCard">
+        <div className="card contaiinerCard">
+          <div className="card-image contaiinerCard">
+            <img className="imgContainer" src={props.img} alt="" />
+          </div>
 
-    return (
-        <div className=" contaiinerCard">
-            <div className="col s12 m7 contaiinerCard">
-                <div className="card contaiinerCard">
-                   
-                    <div className="card-image contaiinerCard">
-                    <img className="imgContainer" src={props.img}/>
-                    </div>
+          <div class=" cardTitle">{props.title}</div>
 
-                    <div class=" cardTitle">
-                        {props.title}
-                    </div>
-                   
-
-                    <div className="card-content contaiinerCard">
-                        <p>{props.descricao}</p>
-                    </div>
-                </div>
-            </div>
+          <div className="card-content contaiinerCard">
+            <p>{props.descricao}</p>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default CardVertical;
