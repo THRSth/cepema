f003264757feee342b7a52e41fa6881fae9d997e
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-11-05 21:02:18 -0300

    Importando fotos de um JSON

--- src/Componentes/Galeria/index.js
+++ src/Componentes/Galeria/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import { Carousel } from "react-materialize";
 
-import foto1 from "../../assets/img/Galeria/foto-1.jpg";
-import foto2 from "../../assets/img/Galeria/foto-2.JPG";
-import foto3 from "../../assets/img/Galeria/foto-3.JPG";
-import foto4 from "../../assets/img/Galeria/foto-4.JPG";
-import foto5 from "../../assets/img/Galeria/foto-5.JPG";
-import foto6 from "../../assets/img/Galeria/foto-6.JPG";
-import foto7 from "../../assets/img/Galeria/foto-7.JPG";
-import foto8 from "../../assets/img/Galeria/foto-8.jpg";
-import foto9 from "../../assets/img/Galeria/foto-9.jpg";
+import fotos from "../../JSON/Galeria";
 
 import "./styles.css";
 
@@ -17,7 +9,7 @@ const Galeria = () => (
   <>
     <Carousel
       className="carousel-container"
-      images={[foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8, foto9]}
+      images={fotos}
       options={{
         fullWidth: true,
         indicators: true,
---
+++ src/JSON/Galeria.js
@@ -0,0 +1,13 @@
+const fotos = [
+  require("../assets/img/Galeria/foto-1.jpg"),
+  require("../assets/img/Galeria/foto-2.JPG"),
+  require("../assets/img/Galeria/foto-3.JPG"),
+  require("../assets/img/Galeria/foto-4.JPG"),
+  require("../assets/img/Galeria/foto-5.JPG"),
+  require("../assets/img/Galeria/foto-6.JPG"),
+  require("../assets/img/Galeria/foto-7.JPG"),
+  require("../assets/img/Galeria/foto-8.jpg"),
+  require("../assets/img/Galeria/foto-9.jpg"),
+];
+
+export default fotos;
